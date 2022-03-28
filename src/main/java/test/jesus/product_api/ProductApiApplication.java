560859f99c7fff997a1358197b2dc0a09e589577
Jesús Alejandro Chang Buccheri <jesusacb@ext.inditex.com>
2022-03-28 20:21:51 +0200

    feature: enable caching

--- src/main/java/test/jesus/product_api/ProductApiApplication.java
+++ src/main/java/test/jesus/product_api/ProductApiApplication.java
@@ -3,11 +3,13 @@ package test.jesus.product_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import test.jesus.product_api.config.ProductAPIConfig;
 import test.jesus.product_api.model.Product;
 
 @SpringBootApplication
 @EnableConfigurationProperties(ProductAPIConfig.class)
+@EnableCaching
 public class ProductApiApplication {
 
 	public static void main(String[] args) {
