b1fbf912e2cc9a2fb23923eabf5548e74a973aff
Jesús Alejandro Chang Buccheri <jesusacb@ext.inditex.com>
2022-03-28 20:28:37 +0200

    feat: Add error handling to ApiClient

--- src/main/java/test/jesus/product_api/repository/ApiClient.java
+++ src/main/java/test/jesus/product_api/repository/ApiClient.java
@@ -1,16 +1,15 @@
 package test.jesus.product_api.repository;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 import test.jesus.product_api.model.ApiResponse;
 import java.time.Duration;
 
 public class ApiClient {
 
-    private int requestTimeoutInSeconds;
-
+    private final int requestTimeoutInSeconds;
     private final WebClient webClient;
 
     ApiClient(String server, int timeout){
@@ -18,18 +17,24 @@ public class ApiClient {
         requestTimeoutInSeconds = timeout;
     }
 
-    public <T> ApiResponse<T> get(Class<T> responseType, String uri){
-
+    public <T> ApiResponse<T> get(Class<T> responseType, String uri) {
         ApiResponse<T> response = new ApiResponse<T>();
-
-        response.body = webClient.get()
-                .uri(uri)
-                .accept(MediaType.APPLICATION_JSON)
-                .exchangeToMono(result -> {
-                    response.status = result.statusCode();
-                    return result.bodyToMono(responseType);
-                }).block(Duration.ofSeconds(requestTimeoutInSeconds));;
-
+        try {
+            response.body = webClient.get()
+                    .uri(uri)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .onStatus(HttpStatus::is4xxClientError,
+                            error -> Mono.error(new RuntimeException("not found")))
+                    .onStatus(HttpStatus::is5xxServerError,
+                            error -> Mono.error(new RuntimeException("internal server error")))
+                    .bodyToMono(responseType)
+                    .block(Duration.ofSeconds(requestTimeoutInSeconds));
+        } catch (RuntimeException re){
+            ApiResponse<T> resp = new ApiResponse<T>();
+            response.body = null;
+            return resp;
+        }
         return response;
     }
 }
